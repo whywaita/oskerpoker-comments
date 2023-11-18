81e7890a51ca18004863db0c7e12ff9e0d8918dc
whywaita <whywaita@whywrite.it>
2023-11-18 18:38:05 +0900

    Divide processor.py

--- web/web.py
+++ web/web.py
@@ -1,4 +1,3 @@
-import time
 import typing
 import os
 import re
@@ -9,7 +8,6 @@ from flask_sqlalchemy import SQLAlchemy
 import yt_dlp
 
 from web.models import db, Queue, Movie
-import movieparser
 
 app = Flask(__name__)
 secret_key = os.environ.get('SECRET_KEY')
@@ -93,59 +91,3 @@ def db_enqueue(db_session: SQLAlchemy, movie_id: str):
 
     db_session.session.add(Queue(movie_id=movie_id))
     db_session.session.commit()
-
-
-def execute_queue():
-    while True:
-        process_queue()
-        time.sleep(1)
-
-
-def process_queue():
-    with app.app_context():
-        queue = db.session.query(Queue).order_by(Queue.id).limit(5).all()
-        if not queue:
-            return
-
-        for q in queue:
-            try:
-                if db.session.query(Movie).filter(Movie.movie_id == q.movie_id).first():
-                    db.session.delete(q)
-                    db.session.commit()
-                    continue
-
-                movie_id = q.movie_id
-                u = f"https://www.youtube.com/watch?v={movie_id}"
-                ydl_opts = {
-                    'outtmpl': 'tmp/%(id)s.%(ext)s',
-                }
-                # Update queue status to processing
-                print(f"Processing start {movie_id}")
-                q.status = 'processing (downloading)'
-                db.session.commit()
-                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                    ydl.download([u])
-                    info_dict = ydl.extract_info(u, download=False)
-                    video_title = info_dict.get('title', None)
-                    file_path = f'tmp/{movie_id}.mp4'
-                    print(f"Downloaded {video_title}")
-
-                    q.status = 'processing (parsing)'
-                    db.session.commit()
-
-                    flop_time_list = movieparser.parse_movie(file_path)
-                    comment = movieparser.get_timestamp_comment_from_list(flop_time_list)
-                    print(f"Parsed {video_title}")
-                    print(comment)
-
-                    db.session.add(Movie(movie_id=movie_id, title=video_title, parsed_text=comment))
-                    db.session.delete(q)
-                    db.session.commit()
-
-                    # Remove tmp file
-                    os.remove(file_path)
-            except (yt_dlp.utils.YoutubeDLError, sqlalchemy.exc.SQLAlchemyError) as e:
-                print(e)
-                q.status = 'error {e}'
-                db.session.commit()
-                continue
