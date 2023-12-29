a2a5c7834109524059754da3b59454360e249a97
whywaita <whywaita@whywrite.it>
2023-12-30 04:51:53 +0900

    Avoid 500 error when AttributeError

--- web/web.py
+++ web/web.py
@@ -35,17 +35,21 @@ def index():
 
 def pretty_text(ms: typing.List[Movie]) -> typing.List[Movie]:
     for movie in ms:
-        pt = movie.parsed_text
-        prettied_text = ''
-        for line in pt.split('\n'):
-            if not line:
-                continue
-            timestamp = re.match(r'(\d+:\d+:\d+)', line).group()
-            players = line.removeprefix(timestamp).strip()
-            h, m, s = map(int, timestamp.split(':'))
-            timestamp_second = h * 3600 + m * 60 + s
-            link = f"https://www.youtube.com/watch?v={movie.movie_id}&t={timestamp_second}s"
-            prettied_text += f'<a href="{link}">{timestamp}</a> {players}<br>'
+        try:
+            pt = movie.parsed_text
+            prettied_text = ''
+            for line in pt.split('\n'):
+                if not line:
+                    continue
+                timestamp = re.match(r'(\d+:\d+:\d+)', line).group()
+                players = line.removeprefix(timestamp).strip()
+                h, m, s = map(int, timestamp.split(':'))
+                timestamp_second = h * 3600 + m * 60 + s
+                link = f"https://www.youtube.com/watch?v={movie.movie_id}&t={timestamp_second}s"
+                prettied_text += f'<a href="{link}">{timestamp}</a> {players}<br>'
+        except AttributeError:
+            print(f'Failed to parse {movie.title}')
+            prettied_text = movie.parsed_text
         movie.parsed_text = prettied_text
     return ms
 
